c87b8b6c4550a6cb0c9fd9143919824f7b864825
Sam Stigler <sstigler1985@mac.com>
2014-03-23 15:10:37 -0500

    Adds HTTP basic authentication support to SYMBambooServer.

--- Symbolicator/Model/Human/SYMBambooServer.h
+++ Symbolicator/Model/Human/SYMBambooServer.h
@@ -2,4 +2,16 @@
 
 @interface SYMBambooServer : _SYMBambooServer {}
 
+/**
+ Sets the username and password for the client to transmit to the server when making requests.
+ @param username Username
+ @param password Password
+ */
+- (void)setUsername:(NSString *)username password:(NSString *)password;
+
+/**
+ @return The HTTP basic authentication header to use when authenticating to this Bamboo server.
+ */
+- (NSDictionary *)HTTPBasicAuthenticationHeader;
+
 @end
--- Symbolicator/Model/Human/SYMBambooServer.m
+++ Symbolicator/Model/Human/SYMBambooServer.m
@@ -1,11 +1,41 @@
-#import "SYMBambooServer.h"
+#import <MagicalRecord/CoreData+MagicalRecord.h>
 
+#import "SYMBambooServer.h"
 
 @interface SYMBambooServer ()
 
 @end
 
+static NSString* const kHTTPAuthorizationHeader = @"Authorization";
 
 @implementation SYMBambooServer
+
+#pragma mark - Authentication
+
+- (void)setUsername:(NSString *)username password:(NSString *)password
+{
+    NSString* authString = [self base64EncodedAuthenticationStringFromUsername:username
+                                                                      password:password];
+    [MagicalRecord saveWithBlockAndWait:^(NSManagedObjectContext *localContext) {
+        self.authenticationString = authString;
+    }];
+}
+
+
+- (NSDictionary *)HTTPBasicAuthenticationHeader
+{
+    NSDictionary* header = @{kHTTPAuthorizationHeader: self.authenticationString};
+    return header;
+}
+
+
+- (NSString *)base64EncodedAuthenticationStringFromUsername:(NSString *)username
+                                                   password:(NSString *)password
+{
+    NSString* authenticationString = [NSString stringWithFormat:@"%@:%@", username, password];
+    NSData* authenticationData = [authenticationString dataUsingEncoding:NSUTF8StringEncoding];
+    NSString* base64EncodedAuthString = [authenticationData base64EncodedStringWithOptions:kNilOptions];
+    return base64EncodedAuthString;
+}
 
 @end
