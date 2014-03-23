dec02bf9c9eb3eb1fe303b163b3c6becbd8871ba
Sam Stigler <sstigler1985@mac.com>
2014-03-23 15:04:45 -0500

    Adds MagicalRecord setup and teardown to app delegate.

--- Symbolicator/SYMAppDelegate.m
+++ Symbolicator/SYMAppDelegate.m
@@ -5,9 +5,21 @@
 //  Created by Sam Stigler on 3/13/14.
 //  Copyright (c) 2014 Sam Stigler. All rights reserved.
 //
+#import <MagicalRecord/CoreData+MagicalRecord.h>
 
 #import "SYMAppDelegate.h"
 
 @implementation SYMAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    [MagicalRecord setupCoreDataStack];
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)notification
+{
+    [MagicalRecord cleanUp];
+}
 
 @end
