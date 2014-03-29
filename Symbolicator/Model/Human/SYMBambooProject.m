a587252067083a9711e5c8b2d13b797f7b099b2b
Sam Stigler <sstigler1985@mac.com>
2014-03-29 18:12:36 -0500

    Implements -[SYMBambooProject prefetchPlans].

--- Symbolicator/Model/Human/SYMBambooProject.m
+++ Symbolicator/Model/Human/SYMBambooProject.m
@@ -1,5 +1,6 @@
+#import "SYMBambooClient+Plans.h"
 #import "SYMBambooProject.h"
-
+#import "SYMNotificationConstants.h"
 
 @interface SYMBambooProject ()
 
@@ -12,5 +13,22 @@
 {
     return @"projects.project";
 }
+
+
+- (void)prefetchPlans
+{
+    [[SYMBambooClient sharedClient]
+     fetchAllPlansForBambooProject:self
+     onServer:self.server
+     withCompletionBlock:^(NSError *error) {
+         if (error != nil)
+         {
+             [[NSNotificationCenter defaultCenter]
+              postNotificationName:SYMBambooPlansUpdatedNotification
+              object:self];
+         }
+     }];
+}
+
 
 @end
